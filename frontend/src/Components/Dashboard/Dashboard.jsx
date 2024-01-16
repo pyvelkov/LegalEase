e6a70a6cb8e092133fecfece2adb66ab877d30b7
Zhivko Velchev <43998646+zhivko95@users.noreply.github.com>
2024-01-15 19:07:53 -0500

    First implementation dynamic tags

--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     ChakraProvider,
     Box,
@@ -9,14 +9,44 @@ import {
 } from "@chakra-ui/react";
 
 const Dashboard = () => {
-    const [formData, setFormData] = useState({
-        date: "",
-        corporationName: "",
-        corporationAddress: "",
-        corporationRepresentativeName: "",
-        representativeTitle: "",
-        targetCorporationName: "",
-    });
+
+    // Set initial empty state
+    const [formData, setFormData] = useState(
+        {});
+
+    const getAllFieldNames = () => { 
+        try {
+            fetch(`${import.meta.env.VITE_SERVER_URL}/wordRoutes/templateTags`,
+                {
+                    method: "GET",
+                    mode: "cors",
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                }).then((res) => {
+                    console.log(res);
+                    return res.json();
+                }).then((data) => {
+
+                    var tagState = {};
+
+                    // Re-structure data from response
+                    data.forEach((tag) => {
+                        tagState[tag.tagName] = {
+                            fieldValue: "",
+                            fieldType: tag.tagType};
+                    });
+            
+                    setFormData(tagState);
+                });
+                
+        } catch (error) {
+            console.error("Error getting template tags:", error);
+        }
+    }
+
+    // Get all unique tags from the template to create input fields for them
+    useEffect(getAllFieldNames(), []);
 
     const handleChange = (e) => {
         setFormData({
@@ -24,6 +54,7 @@ const Dashboard = () => {
             [e.target.name]: e.target.value,
         });
     };
+
     const handleEditWordFile = async () => {
         try {
             const response = await fetch(
@@ -48,71 +79,34 @@ const Dashboard = () => {
     return (
         <ChakraProvider>
             <Box p={4}>
-                <FormControl>
-                    <FormLabel>Date</FormLabel>
-                    <Input
-                        type="text"
-                        name="date"
-                        value={formData.date}
-                        onChange={handleChange}
-                        placeholder="Enter date"
-                    />
-                </FormControl>
 
-                <FormControl>
-                    <FormLabel>Corporation Name</FormLabel>
-                    <Input
-                        type="text"
-                        name="corporationName"
-                        value={formData.corporationName}
-                        onChange={handleChange}
-                        placeholder="Enter Corporation Name"
-                    />
-                </FormControl>
-
-                <FormControl mt={4}>
-                    <FormLabel>Corporation Address</FormLabel>
-                    <Input
-                        type="text"
-                        name="corporationAddress"
-                        value={formData.corporationAddress}
-                        onChange={handleChange}
-                        placeholder="Enter Corporation Address"
-                    />
-                </FormControl>
-
-                <FormControl mt={4}>
-                    <FormLabel>Corporation Representative Name</FormLabel>
-                    <Input
-                        type="text"
-                        name="corporationRepresentativeName"
-                        value={formData.corporationRepresentativeName}
-                        onChange={handleChange}
-                        placeholder="Enter Corporation Representative Name"
-                    />
-                </FormControl>
-
-                <FormControl mt={4}>
-                    <FormLabel>Representative Title</FormLabel>
-                    <Input
-                        type="text"
-                        name="representativeTitle"
-                        value={formData.representativeTitle}
-                        onChange={handleChange}
-                        placeholder="Enter Representative Title"
-                    />
-                </FormControl>
-
-                <FormControl mt={4}>
-                    <FormLabel>Target Corporation Name</FormLabel>
-                    <Input
-                        type="text"
-                        name="targetCorporationName"
-                        value={formData.targetCorporationName}
-                        onChange={handleChange}
-                        placeholder="Enter Target Corporation Name"
-                    />
-                </FormControl>
+                {// If tags have been retrieved from backend, render the textboxes.
+                formData ?
+                    (Object.keys(formData).map((fieldName) => (
+                        <FormControl>
+                            <FormLabel>{fieldName}</FormLabel>
+                            <Input
+                                type={formData[fieldName].fieldType}
+                                name={fieldName}
+                                value={formData[fieldName].fieldValue}
+                                onChange={handleChange}
+                                placeholder={"Enter value for " + fieldName}
+                            />
+                        </FormControl>
+                    )))
+                    :
+                    // Else, display placeholder
+                    // TODO - Implement loading bar/wheel, etc. while we wait for field data.
+                    (<FormControl>
+                            <FormLabel>PlaceHolder</FormLabel>
+                            <Input
+                                type="text"
+                                name="Placeholder"
+                                value="Placeholder"
+                                onChange={handleChange}
+                                placeholder="Enter value for Placeholder"
+                            />
+                        </FormControl>)}
 
                 <Button mt={4} colorScheme="blue" onClick={handleEditWordFile}>
                     Create Document
