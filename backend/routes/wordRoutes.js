9576c9667427c194a90679a90d7d232e3ca40d84
Plamen <55108855+pyvelkov@users.noreply.github.com>
2024-01-15 19:29:57 -0500

    Dynamic test (#1)
    * First implementation dynamic tags
    
    * added dynamic tag field generation based on template tags
    
    ---------
    
    Co-authored-by: Zhivko Velchev <43998646+zhivko95@users.noreply.github.com>

--- backend/routes/wordRoutes.js
+++ backend/routes/wordRoutes.js
@@ -10,6 +10,50 @@ const __dirname = dirname(__filename);
 
 const router = Router();
 
+router.get("/templateTags", (req, res) => {
+
+    // TODO: Implement template UUIDs
+
+    // Read in template file
+    // TODO: Add all this boiletplate docxtemplater code into utility function.
+    const templatePath = resolve(
+        __dirname,
+        "../files/templates/Retainer Agreement - Corporate (for Corporation).docx"
+    );
+    const template = readFileSync(templatePath, "binary");
+
+    // Unzip the content of the file since word files are considered zipped files.?!
+    const zip = new PizZip(template);
+
+    // This will parse the template, and will throw an error if the template is
+    // invalid, for example, if the template is "{firstName" (no closing tag)
+    // === May need additional error handling ===
+    const doc = new Docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true,
+    });
+
+    const fullDocText = doc.getFullText();
+
+    // Apply regular expression to full document text and filter for unique matches only
+    const uniqueDocTags = fullDocText.match(/({\w+})/g).filter(
+        (value, index, array) => {
+            return array.indexOf(value) === index
+        });
+
+    // Structure response JSON object
+    let tags = [];
+    uniqueDocTags.forEach((tag) => {
+        tags.push({
+            tagName: tag.slice(1, -1),
+            tagType: "text"
+        });
+    });
+
+    res.json(tags);
+
+});
+
 router.post("/editWordFile", (req, res) => {
     const templatePath = resolve(
         __dirname,
