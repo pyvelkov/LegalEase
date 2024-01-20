2e19921189c28fdcaf4829afd5ffb907bc3f843a
pyvelkov <plamen.velkov@hotmail.com>
2024-01-20 16:23:40 -0500

    added frontend to backend file uploading via route using uuid

--- backend/routes/templateRoutes.js
+++ backend/routes/templateRoutes.js
@@ -13,31 +13,28 @@ const fileUpload = multer();
 
 // Download template file
 router.get("/:uuid", async (req, res) => {
-
     const dbClient = new pg.Client();
     await dbClient.connect();
 
     const sqlQuery = {
         text: 'select * from public."TEMPLATES" where "TMP_UUID" = $1',
-        values: [req.params.uuid]
+        values: [req.params.uuid],
     };
 
     try {
         const dbRes = await dbClient.query(sqlQuery);
         // This is where we will return useful data later on once file i/o works.
-        res.send("Template downloaded successfully.");
+        res.status(200).send("Template downloaded successfully.");
     } catch (error) {
         console.error(error);
         res.status(500).send("Error downloading template: " + error);
     } finally {
         await dbClient.end();
     }
-
 });
 
 // Upload a template
-router.post("/:uuid", fileUpload.single('templateFile'), async (req, res) => {
-
+router.post("/:uuid", fileUpload.single("templateFile"), async (req, res) => {
     const templatePath = `templates/${req.params.uuid}`;
     //const templateFile = req.file;
 
@@ -52,7 +49,6 @@ router.post("/:uuid", fileUpload.single('templateFile'), async (req, res) => {
         process.exitCode = 1;
       })*/
 
-
     // Establish new database connection
     const dbClient = new pg.Client();
     await dbClient.connect();
@@ -60,20 +56,19 @@ router.post("/:uuid", fileUpload.single('templateFile'), async (req, res) => {
     // Construct SQL query to insert new template record
     const sqlQuery = {
         text: 'INSERT INTO public."TEMPLATES" VALUES ($1, $2, $3)',
-        values: [req.params.uuid, req.body.templateName, templatePath]
+        values: [req.params.uuid, req.body.templateName, templatePath],
     };
 
     // Execute query in database and wait for success/fail response
     try {
         const dbRes = await dbClient.query(sqlQuery);
-        res.send("Template uploaded successfully.");
+        res.status(200).send("Template uploaded successfully.");
     } catch (error) {
         console.error(error);
         res.status(500).send("Error uploading template: " + error);
     } finally {
         await dbClient.end();
     }
-
 });
 
 export default router;
