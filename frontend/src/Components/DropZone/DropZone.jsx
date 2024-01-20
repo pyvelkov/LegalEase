8be29215f523f47a6143f5df331cbac0df2c7c30
Plamen <55108855+pyvelkov@users.noreply.github.com>
2024-01-20 16:27:21 -0500

    added frontend to backend file uploading via route using uuid (#25)
    Looks good

--- frontend/src/Components/DropZone/DropZone.jsx
+++ frontend/src/Components/DropZone/DropZone.jsx
@@ -1,18 +1,41 @@
 import { useDropzone } from "react-dropzone";
 import { Box, Text, Center } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 
 const DropZone = () => {
-    const onDrop = (acceptedFiles) => {
-        acceptedFiles.forEach((file) => {
-            console.log("Uploaded file:", file.name);
-        });
+    const onDrop = async (acceptedFiles) => {
+        try {
+            const formData = new FormData();
+            formData.append("templateFile", acceptedFiles[0]);
+            formData.append("templateName", acceptedFiles[0].name);
+
+            const response = await fetch(
+                `${import.meta.env.VITE_SERVER_URL}/template/${uuidv4()}`,
+                {
+                    method: "POST",
+                    body: formData,
+                }
+            );
+
+            if (response.ok) {
+                console.log("Server got the file");
+            } else {
+                console.log(
+                    "Something went wrong with file uploading (Server-side)."
+                );
+            }
+
+            // acceptedFiles.forEach((file) => {
+            //     console.log("Uploaded file:", file);
+            // });
+        } catch (err) {
+            console.log("Error uploading file: ", err);
+        }
     };
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         onDrop,
     });
-
-    // const textLabel = `Drag and drop document here or click to select it!`;
 
     return (
         <Center>
