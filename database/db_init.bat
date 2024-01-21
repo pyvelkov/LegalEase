fcef223e998e2d10f6ae96cb301413ead2e070f8
Zhivko Velchev <43998646+zhivko95@users.noreply.github.com>
2024-01-21 02:13:16 -0500

    remove forgotten comments in install script

--- database/db_init.bat
+++ database/db_init.bat
@@ -5,9 +5,3 @@ set PGPASSWORD=admin
 @echo on
 
 for /r %%f in (*.sql) do "C:\Program Files\PostgreSQL\16\bin\psql.exe" -U %PGUSER% -d %PGDATABASE% -f %%f
-
-:: date column in templates table
-:: route to return all templates
-:: reoute to return tag fields for single template
-:: reoute for deleting template
-:: database table to store template tags
