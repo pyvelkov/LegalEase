c4383339a3f787f8e39ad6964389dad9d4c986ec
Zhivko Velchev <43998646+zhivko95@users.noreply.github.com>
2024-01-24 10:04:09 -0500

    Issues (20,29,33) - Backend Routes, Tag Processing Logic and GCS storage implementation (#34)
    * added GET and POST routes for templates by UUID \n added logic to process different tags and return names, types and options \n set up GCS for filestorage
    
    * added GET and POST routes for templates by UUID
    added logic to process different tags and return names, types and options
    set up GCS for filestorage

--- backend/routes/templateRoutes.js
+++ backend/routes/templateRoutes.js
@@ -4,6 +4,8 @@ import { dirname, resolve } from "path";
 import multer from "multer";
 import pg from "pg";
 import * as storage from "@google-cloud/Storage";
+import { Readable } from "stream";
+import { processTemplateFields, getTemplateFields } from "../util/docUtils.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,61 +13,92 @@ const __dirname = dirname(__filename);
 const router = Router();
 const fileUpload = multer();
 
-// Download template file
+/* =================
+ * Get template metadata (fields, date, etc.)
+ * ================= */
 router.get("/:uuid", async (req, res) => {
+
+    // Establish new database connection
     const dbClient = new pg.Client();
     await dbClient.connect();
 
+    // Configure SQL query to select all required template metadata
     const sqlQuery = {
-        text: 'select * from public."TEMPLATES" where "TMP_UUID" = $1',
+        text: "select TMP_UUID, TMP_NAME, TMP_DATE_CREATED, TMPF_FIELDS \
+                from public.TEMPLATES join public.TEMPLATE_FIELDS \
+                on TMP_UUID = TMPF_TMP_UUID \
+                where TMP_UUID = CAST ($1 as UUID)",
         values: [req.params.uuid],
     };
 
+    // Execute queries in database and wait for success/fail response
+    let dbRes;
     try {
-        const dbRes = await dbClient.query(sqlQuery);
-        // This is where we will return useful data later on once file i/o works.
-        res.status(200).send("Template downloaded successfully.");
+        dbRes = await dbClient.query(sqlQuery);
     } catch (error) {
         console.error(error);
         res.status(500).send("Error downloading template: " + error);
     } finally {
         await dbClient.end();
     }
+
+    // Return 404 if no records found, otherwise should only be 1 record
+    dbRes.rowCount > 0
+    ? res.status(200).json(dbRes.rows[0]) 
+    : res.status(404).send();
+
+
 });
 
-// Upload a template
-router.post("/:uuid", fileUpload.single("templateFile"), async (req, res) => {
-    const templatePath = `templates/${req.params.uuid}`;
-    //const templateFile = req.file;
+/* =================
+ * Upload a template
+ * ================= */
+ router.post("/:uuid", fileUpload.single("templateFile"), async (req, res) => {
+    const templatePath = `templates/${req.params.uuid}/${req.file.originalname}`;
+    const templateFile = req.file.buffer;
 
     // Create new storage client for GCS and upload file to path in bucket
-    /*const storageClient = new storage.Storage();
-    await storageClient.bucket("legalease").file(templatePath).save(templateFile);
+    try{
+        const storageClient = new storage.Storage();
+        await storageClient.bucket("legalease").file(templatePath).save(Readable.from(templateFile));
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).send("Error uploading template file to GCS.")
+    }
 
-    uploadFromMemory().catch(console.error);
+    // Construct TEMPLATE SQL query to insert new template record
+    const templateUuid = req.params.uuid, 
+        templateName = req.body.templateName,
+        templateDateCreated = Date.now(); // Divide by 100 later to convert ms to s
+        
+    const templateSqlQuery = {
+        text: 'insert into public.TEMPLATES values ($1, $2, $3, to_timestamp($4/1000.0))',
+        values: [templateUuid, templateName, templatePath, templateDateCreated],
+    };
 
-    process.on('unhandledRejection', err => {
-        console.error(err.message);
-        process.exitCode = 1;
-      })*/
+    // Get all available template fields
+    const templateFields = getTemplateFields(templateFile);
+
+    // Construct TEMPLATE_FIELDS SQL query to insert new template fields record.
+    const templateFieldSqlQuery = {
+        text: 'insert into public.TEMPLATE_FIELDS values ($1, $2)',
+        values: [templateUuid, JSON.stringify(templateFields)],
+    };
 
     // Establish new database connection
     const dbClient = new pg.Client();
     await dbClient.connect();
 
-    // Construct SQL query to insert new template record
-    const sqlQuery = {
-        text: 'INSERT INTO public."TEMPLATES" VALUES ($1, $2, $3)',
-        values: [req.params.uuid, req.body.templateName, templatePath],
-    };
-
-    // Execute query in database and wait for success/fail response
+    // Execute queries in database and wait for success/fail response
     try {
-        const dbRes = await dbClient.query(sqlQuery);
+        const templateQueryRes = await dbClient.query(templateSqlQuery);
+        const templateFieldQueryRes = await dbClient.query(templateFieldSqlQuery);
         res.status(200).send("Template uploaded successfully.");
     } catch (error) {
+        // Consider adding code here to delete template file if DB write failed.
         console.error(error);
-        res.status(500).send("Error uploading template: " + error);
+        res.status(500).send("Error writing template records to DB: " + error);
     } finally {
         await dbClient.end();
     }
--- backend/util/docUtils.js
+++ backend/util/docUtils.js
@@ -2,13 +2,13 @@ import { readFileSync, writeFileSync } from "fs";
 import Docxtemplater from "docxtemplater";
 import PizZip from "pizzip";
 
+// TODO: make this work with a file buffer instead of path
 // Gets template file from path and returns a new DOCXTemplater object.
 // ==> Consider adding optional parameters for docxtemplater options.
-const getTemplateFromPath = (path) => {
-    const template = readFileSync(path, "binary");
+const getTemplateFromFile = (file) => {
 
     // Unzip the content of the file since word files are considered zipped files.?!
-    const zip = new PizZip(template);
+    const zip = new PizZip(file);
 
     // This will parse the template, and will throw an error if the template is
     // invalid, for example, if the template is "{firstName" (no closing tag)
@@ -19,4 +19,86 @@ const getTemplateFromPath = (path) => {
     });
 };
 
-export { getTemplateFromPath };
+// Handle all tag type detection
+const processTemplateFields = (uniqueFields) => {
+
+    const availableFieldTypes = ["dropdown", "text", "date", "num"];
+    let fields = [];
+
+    uniqueFields.map((field) => {
+
+        // Initialize name, type and options for each field
+        let name = "", type = "", options = [];
+
+        // Drop curly braces at beginning and end and split by chunks
+        const splitResult = field.slice(1, -1).split("_");
+        
+        // Determine the type of field
+        if (availableFieldTypes.includes(splitResult[0].toLowerCase())){
+            type = splitResult[0];
+        }
+        else {
+            // If not a standard field type
+            type = "INVALID";
+        }
+
+        // Based on the type of field, find the field name and options (if applicable)
+        switch (type){
+            
+            // Dropdown field type is special case since it contains list of options
+            case "dropdown":
+                
+                // Take the whole tag after the first type identifier section
+                // Name is everything up until first quote char
+                // Everything after is options 
+                splitResult.slice(1).map((tagSection) => {
+                    if (!tagSection.includes("\"")){
+                        name += " " + tagSection;
+                    }
+                    else {
+                        options.push(tagSection.slice(1, -1));
+                    }
+                });
+                break;
+            
+            case "INVALID":
+                name = "INVALID";
+                options = "INVALID";
+                break;
+
+            // For TEXT, DATE and NUM
+            default:
+                name = splitResult.slice(1).join(" ");
+                options = "";
+
+        }  
+
+        fields.push({
+            fieldName: name.trim(),
+            fieldType: type,
+            fieldOptions: options
+        });
+    });
+
+    return fields;
+};
+
+// TODO: Call from template/:uuid/tags route
+// Returns all unique tags from the given template
+const getTemplateFields = (templateFile) => {
+
+    const doc = getTemplateFromFile(templateFile);
+
+    const fullDocText = doc.getFullText();
+
+    // Apply regular expression to full document text and filter for unique matches only
+    const uniqueDocFields = fullDocText.match(/({.[^}]+})/g).filter(
+        (value, index, array) => {
+            return array.indexOf(value) === index
+        }); 
+
+    return processTemplateFields(uniqueDocFields);
+};
+
+// TODO: change this to export function that returns tags instead
+export { processTemplateFields, getTemplateFields };
