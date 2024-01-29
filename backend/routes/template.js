cad575e103184918d6c75e7c6b84ddfd1184dd21
Zhivko Velchev <43998646+zhivko95@users.noreply.github.com>
2024-01-29 17:56:30 -0500

    Issue 37: Create backend route to retrieve all available templates and their metadata (#41)
    * added new backend route to get ALL templates + small cleanup + small naming changes
    
    * comment correction from 404 to 204
    
    * changed some single-quotes to double-quotes for standards
    
    * added missing line ending semi-colon

--- backend/routes/template.js
+++ backend/routes/template.js
@@ -1,28 +1,59 @@
 import { Router } from "express";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
 import multer from "multer";
 import pg from "pg";
-import * as storage from "@google-cloud/Storage";
+import * as storage from "@google-cloud/storage";
 import { Readable } from "stream";
-import { processTemplateFields, getTemplateFields } from "../util/docUtils.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { getTemplateFields } from "../util/docUtils.js";
 
 const router = Router();
 const fileUpload = multer();
 
 /* =================
- * Get template metadata (fields, date, etc.)
+ * Get ALL templates metadata (fields, date, etc.)
+ * ================= */
+router.get("/", async (req, res) => {
+
+    // Establish new database connection
+    const dbClient = new pg.Client();
+    await dbClient.connect();
+
+    // Configure SQL query to select template metadata for all available templates
+    const templateSqlQuery = {
+        text: "select TMP_UUID, TMP_NAME, TMP_DATE_CREATED, TMPF_FIELDS \
+                from public.TEMPLATES join public.TEMPLATE_FIELDS \
+                on TMP_UUID = TMPF_TMP_UUID",
+        values: [],
+    };
+
+    // Execute queries in database and wait for success/fail response
+    let dbRes;
+    try {
+        dbRes = await dbClient.query(templateSqlQuery);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error getting ALL templates metadata from DB: " + error);
+    } finally {
+        await dbClient.end();
+    }
+
+    // Return 204 if no records found
+    dbRes.rowCount > 0
+    ? res.status(200).json({templates: dbRes.rows}) 
+    : res.status(204).send();
+
+});
+
+/* =================
+ * Get specific template metadata (fields, date, etc.)
  * ================= */
 router.get("/:uuid", async (req, res) => {
+
     // Establish new database connection
     const dbClient = new pg.Client();
     await dbClient.connect();
 
     // Configure SQL query to select all required template metadata
-    const sqlQuery = {
+    const templateSqlQuery = {
         text: "select TMP_UUID, TMP_NAME, TMP_DATE_CREATED, TMPF_FIELDS \
                 from public.TEMPLATES join public.TEMPLATE_FIELDS \
                 on TMP_UUID = TMPF_TMP_UUID \
@@ -33,44 +64,44 @@ router.get("/:uuid", async (req, res) => {
     // Execute queries in database and wait for success/fail response
     let dbRes;
     try {
-        dbRes = await dbClient.query(sqlQuery);
+        dbRes = await dbClient.query(templateSqlQuery);
     } catch (error) {
         console.error(error);
-        res.status(500).send("Error downloading template: " + error);
+        res.status(500).send("Error getting template metadata from DB: " + error);
     } finally {
         await dbClient.end();
     }
 
     // Return 404 if no records found, otherwise should only be 1 record
     dbRes.rowCount > 0
-        ? res.status(200).json(dbRes.rows[0])
-        : res.status(404).send();
+    ? res.status(200).json(dbRes.rows[0]) 
+    : res.status(404).send();
+
+
 });
 
 /* =================
  * Upload a template
  * ================= */
-router.post("/:uuid", fileUpload.single("templateFile"), async (req, res) => {
+ router.post("/:uuid", fileUpload.single("templateFile"), async (req, res) => {
     const templatePath = `templates/${req.params.uuid}/${req.file.originalname}`;
     const templateFile = req.file.buffer;
 
     // Create new storage client for GCS and upload file to path in bucket
-    try {
+    try{
         const storageClient = new storage.Storage();
-        await storageClient
-            .bucket("legalease")
-            .file(templatePath)
-            .save(Readable.from(templateFile));
-    } catch (error) {
+        await storageClient.bucket("legalease").file(templatePath).save(Readable.from(templateFile));
+    }
+    catch(error){
         console.error(error);
         res.status(500).send("Error uploading template file to GCS.");
     }
 
     // Construct TEMPLATE SQL query to insert new template record
-    const templateUuid = req.params.uuid,
+    const templateUuid = req.params.uuid, 
         templateName = req.body.templateName,
         templateDateCreated = Date.now(); // Divide by 100 later to convert ms to s
-
+        
     const templateSqlQuery = {
         text: "insert into public.TEMPLATES values ($1, $2, $3, to_timestamp($4/1000.0))",
         values: [templateUuid, templateName, templatePath, templateDateCreated],
@@ -92,9 +123,7 @@ router.post("/:uuid", fileUpload.single("templateFile"), async (req, res) => {
     // Execute queries in database and wait for success/fail response
     try {
         const templateQueryRes = await dbClient.query(templateSqlQuery);
-        const templateFieldQueryRes = await dbClient.query(
-            templateFieldSqlQuery
-        );
+        const templateFieldQueryRes = await dbClient.query(templateFieldSqlQuery);
         res.status(200).send("Template uploaded successfully.");
     } catch (error) {
         // Consider adding code here to delete template file if DB write failed.
